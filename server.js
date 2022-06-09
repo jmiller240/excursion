a75440bf27b365d81f07986ec4c247842b76e8f8
Jack <Miller>
2022-06-09 16:23:20 -0400

    first attempt to add index.html route

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false, }));
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+});
+
+app.get('/funky', (req, res) => {
+    
+    res.send();
+});
+
+app.listen(3000, () => console.log(`Server started on 3000`));
