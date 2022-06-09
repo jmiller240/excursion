11a955ec7ff837c921975b5a478c9bfc500312cd
Jack <Miller>
2022-06-09 16:45:23 -0400

    feature working

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }));
+app.use(express.static('./'));
 
 app.get('/', (req, res) => {
-    res.send('Hi there');
+    res.send(`<h1 style='font-size: 100px;'>Hi there</h1>`);
 });
 
-app.get('/funkyfunky', (req, res) => {
-    res.sendFile('./index.html');
+app.get('/funky', (req, res) => {
+    res.sendFile('./index.html', { root: __dirname });
 });
 
 app.listen(3000, () => console.log(`Server started on 3000`));
